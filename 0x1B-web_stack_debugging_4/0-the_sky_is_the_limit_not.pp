7a29d328a7c7d0121ae115545df8afcc3c186589
hannahochim <hannahochim@gmail.com>
2024-08-22 17:12:20 -0700

    debug

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,6 @@
-# Fix open files
-exec { 'Fix hard limit':
-  command => '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf'
+# fix our stack so that there is not failed requests
+
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
 }
